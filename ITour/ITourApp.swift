e9ec7d1a721b051af953a023ae3420f91f672e31
yakup2525 <yakup_okumus@outlook.com>
2024-08-25 16:37:20 +0300

    Initial Commit

---
+++ ITour/ITourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITourApp.swift
+//  ITour
+//
+//  Created by Wolf Trail on 25.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ITourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
