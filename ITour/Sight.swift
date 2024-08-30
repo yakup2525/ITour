16625b48d2d968b358a19a4cff9fccdf5b3e5870
yakup2525 <yakup_okumus@outlook.com>
2024-08-30 21:37:40 +0300

    Sight Added

---
+++ ITour/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  ITour
+//
+//  Created by Wolf Trail on 30.08.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
